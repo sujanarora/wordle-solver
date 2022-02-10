9d32646aef7efc64a3eeb85648a442d140856f00
Sujan Arora <sujana@umich.edu>
2022-02-10 07:20:52 -0500

    working version

--- wordle.cpp
+++ wordle.cpp
@@ -9,6 +9,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+
 #include <iomanip>
 #include "xcode_redirect.hpp"
 
@@ -36,30 +37,109 @@ struct Wordle{
 }; // Wordle
 
 void Wordle::printGuesses(){
-    
+    cout << "word  | language frequency\n--------------------\n";
+    for (unsigned i = 0; i < v.size(); ++i){
+        cout << v[i].word << " | " << v[i].freq << "\n";
+        if (i >= 20) break;
+    }
 }
+
+void Wordle::eliminateData(){
+    vector<char> allowRepeatLetters;
+    for (unsigned i = 0; i < currColor.size(); ++i){
+        vector<WordWeight> wordslist;
+        if (currColor[i] == 'g'){
+            allowRepeatLetters.push_back(currWord[i]);
+            for (unsigned j = 0; j < v.size(); ++j){
+                if (v[j].word[i] == currWord[i]){
+                    wordslist.push_back(v[j]);
+                } // if word has green char at correct position
+            } // for j: each word in v
+        } // green
+        else if (currColor[i] == 'y'){
+            allowRepeatLetters.push_back(currWord[i]);
+            for (unsigned j = 0; j < v.size(); ++j){
+                char mightHave = currWord[i]; // right letter, wrong pos
+                if (v[j].word[i] == mightHave){
+                    continue; // word in the wrong spot
+                } // if word has green char at correct position
+                bool shouldPushBack = false;
+                for (unsigned k = 0; k < currWord.size(); ++k){
+                    if (v[j].word[k] == mightHave){
+                        shouldPushBack = true;
+                    }
+                } // for k: chars in currWord
+                if (shouldPushBack) wordslist.push_back(v[j]);
+            } // for j: words in v
+        } // yellow
+        else if (currColor[i] == 'x'){
+            bool letterInWord = false;
+            for (unsigned l = 0; l < allowRepeatLetters.size(); ++l){
+                if (currWord[i] == allowRepeatLetters[l]) letterInWord = true;
+            }
+            if (letterInWord) break;
+            for (unsigned j = 0; j < v.size(); ++j){
+                char doesntHave = currWord[i]; // right letter, wrong pos
+                bool shouldPushBack = true;
+                for (unsigned k = 0; k < currWord.size(); ++k){
+                    if (v[j].word[k] == doesntHave){
+                        shouldPushBack = false;
+                    }
+                } // for k: chars in currWord
+                if (shouldPushBack) wordslist.push_back(v[j]);
+            } // for j: words in v
+        } // gray
+        else{
+            cout << "Error: invalid input";
+            exit(1);
+        }
+        v = wordslist;
+    } // for i: chars in currColor
+    
+} // eliminateData()
 
 void Wordle::getGuesses(){
     if (turnCounter == 1){
         cout << "Make an initial guess. Suggestions:\n"
         << "1. raine\n2. arose\n3. arioel\n4. orate\n5. irate\n6. arise\n7. soare\n8. raile\n9. raise\n10. roate\n .\n .\n .\n"
-        <<"Type guess (ex. 'soare'):\n";
+        <<"Type guess (ex. 'soare'):\n> ";
         /*source: https://artofproblemsolving.com/news/articles/the-math-of-winning-wordle*/
     }
     else{
-        cout << "Turn " << turnCounter << "Best words:\n";
+        cout << "Turn " << turnCounter << " - Best words:\n";
+        printGuesses();
+        cout << "Type guess (ex. 'soare')\nIf wordle is complete type 'done'\n> ";
     }
     
 
-    cout << "> ";
+    
     cin >> currWord;
     
-    cout << "\nGuess 1: " << currWord << ". Type position information\n"
+    if (currWord == "done") exit(0);
+    
+//    bool validWord = false;
+//    for (unsigned i = 0; i < v.size(); ++i){
+//        if (currWord == v[i].word) validWord = true;
+//    }
+//    if (!validWord){
+//        cout << "Error: invalid input";
+//        exit(1);
+//    }
+    if (currWord.size() != 5){
+        cout << "Error: invalid input";
+        exit(1);
+    }
+    
+    cout << "\nGuess: " << currWord << ". Type position information\n"
     << BOLDGREEN << "g"<< RESET << " = green, "<< BOLDYELLOW << "y" << RESET << " = yellow, x = gray "<<
     "(ex. x"<< BOLDGREEN << "g" << BOLDYELLOW << "yy" << RESET << "x)\n";
     
     cout << "> ";
     cin >> currColor;
+    
+    
+    eliminateData();
+    
     ++turnCounter;
 } // getGuesses()
 
@@ -80,7 +160,7 @@ int main(/*int argc, const char ** argv*/){
     w.v.resize(12972);
     std::string s;
     long double f;
-    size_t counter = 0;
+    unsigned counter = 0;
     while (file >> s >> f){
         w.v[counter].word = s;
         w.v[counter].freq = f;
